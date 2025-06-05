dd04d5bfde088370a40583a92877988fbcd3860d
Yash Bidvi <yashbidvi2024@email.com>
2025-06-05 10:33:24 +0530

    i learned function

---
+++ hello.js
@@ -0,0 +1,3 @@
+const prompt = require('prompt-sync')();
+const name=prompt("What is your name?");
+console.log("Hello, "+ name + "!");
---
+++ world.js
@@ -0,0 +1,6 @@
+function sq(x){
+        let ans=0;
+        ans=x-x;
+        return ans;
+}
+console.log(sq(2));
